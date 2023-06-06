f891a1c409879c6d9deabb775fe92106585aaa44
Daeho Choi <112150244+DaeHOHoHOHo@users.noreply.github.com>
2023-06-06 23:44:00 +0900

    ReadME changed

---
+++ Game_Shigan/Source/Game_Shigan/MyAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "MyAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_SHIGAN_API UMyAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project_Shigan
+
 3D 로그라이크 Game
+<Br>
+Game_Shigan : 폐기<br>
+사유 : 무기 시스템 망가져서 회복 불가
